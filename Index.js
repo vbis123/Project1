ec5572ec73351d68e070d49b968d553aa34a3836
Владимир <vbis123@hotmail.com>
2026-07-27 16:43:43 +0300

    Цикл

---
+++ Index.js
@@ -0,0 +1,10 @@
+// Простой цикл: вывести числа от 1 до 10
+for (let i = 1; i <= 10; i++) {
+	console.log(i);
+}
+
+// Пример с массивом
+const items = ['яблоко', 'банан', 'вишня'];
+for (const item of items) {
+	console.log(item);
+}
